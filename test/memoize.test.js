00034b3eda61e7514d336dd1880bf23a67ffea68
olivena <olivia.takkinen@gmail.com>
2024-12-05 16:26:48 +0200

    trial of test

--- test/memoize.test.js
+++ test/memoize.test.js
@@ -1,2 +1,10 @@
-//var chai = require("chai")
-//var memoize = from("../src/memoize.js")
+import chai from "chai"
+import memoize from "../src/memoize.js"
+
+const expect = chai.expect
+
+describe("test of tests", () => {
+    it("trial", () =>{
+        expect()
+    })
+})
