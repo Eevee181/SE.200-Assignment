0b5ab40b5b74f7d443ef43f2de37c40655ba1e09
olivena <olivia.takkinen@gmail.com>
2024-12-06 18:47:56 +0200

    words test file created

---
+++ test/words.test.js
@@ -0,0 +1,11 @@
+import chai from "chai"
+import words from "../src/words.js"
+
+const expect = chai.expect
+
+//not valid test
+describe("sentence or sequence is splitted to an array", () => {
+    it("Sentence is splitted, array doesn't contain other than words", () =>{
+        expect(words('fred, barney, & pebbles')).to.equal('fred', 'barney', 'pebbles') 
+    })
+})
