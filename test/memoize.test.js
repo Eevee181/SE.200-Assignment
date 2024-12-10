addf19658b04c9e1da6b91c85b3f15498e9e2b8e
olivena <olivia.takkinen@gmail.com>
2024-12-10 21:46:18 +0200

    negative testcase

--- test/memoize.test.js
+++ test/memoize.test.js
@@ -2,9 +2,12 @@ import chai from "chai"
 import memoize from "../src/memoize.js"
 
 const expect = chai.expect
+//const should = chai.should
 
-describe("test of tests", () => {
-    it("trial", () =>{
-        expect()
+describe("memoize works", () => {
+    it("memoize is called with wrong variables", () => {
+        var a = "A"
+        var b = "B"
+        expect(memoize(a, b)).Throw(new TypeError('Expected a function'))
     })
 })
