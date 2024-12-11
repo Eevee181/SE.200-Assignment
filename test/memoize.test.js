99c4c47b97c363526ea0f55858a5357db105d671
olivena <olivia.takkinen@gmail.com>
2024-12-11 14:41:57 +0200

    addded memoize test

--- test/memoize.test.js
+++ test/memoize.test.js
@@ -1,13 +1,19 @@
 import chai from "chai"
 import memoize from "../src/memoize.js"
+import add from "../src/add.js"
 
 const expect = chai.expect
 //const should = chai.should
 
-describe("memoize works", () => {
+describe("memoize", () => {
     it("memoize is called with wrong variables", () => {
         var a = "A"
         var b = "B"
         expect(memoize(a, b)).Throw(new TypeError('Expected a function'))
     })
+    
+    it("function is called with function", () => {
+        expect(memoize(add(3,4),add(6,5))).to.equal(add(3,4))
+    })
+    ;
 })
